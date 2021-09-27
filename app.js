759a77b5d8368fc1b19c401f25313bc708deaed3
Marcos Bravo <76933913+mbraaavo@users.noreply.github.com>
2021-09-27 12:50:41 -0600

    Sum JavaScript Fiddling...
    Was able to get the "add-item" functionality done! Having some issues with the "done" and "remove" button functionality. Will look up DevEds to-do list tutorial for pointers on separating those two properly. Once the JS functionality is done i'll be styling it way more! Nicer fonts and using the animated gradient from the reddit post i found.

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
-function addItem () {
+//Grab some DOM stuff :)
+let user_input = document.querySelector('#input');
+let add_button = document.querySelector('#add_button');
+let main_field = document.querySelector('.main_field');
+
+
+add_button.addEventListener("click", () =>{
+    let to_do = user_input.value;
+
+    let html = '';
+    html += '<div class="item-box">';
+    html += '<div class="item">';
+    html += '<p class="to-do">'+ to_do +'</p>';
+    html += '</div>';
+    html += '<div class="clear">';
+    html += '<button class="done_button">O</button>';
+    html += '<button class="clear_button">X</button>';
+    html += '</div>';
+    html += '</div>';    
+
+    main_field.innerHTML += html;
+
+    user_input.value = '';
+
+    let done_button = document.querySelector('.done_button');
+    let clear_button = document.querySelector('.clear_button');
+
+    done_button.addEventListener("click", () =>{
+        console.log("ASSSUUUUUUUUH");
+    });
     
-};
+    clear_button.addEventListener("click", () =>{
+        console.log("DUUUUUUUUUUUUDE");
+    });
+    
+
+});
+
